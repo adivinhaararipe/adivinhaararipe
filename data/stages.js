e132a7f79ede3a4497045410623826f4eff43eb0
Renan <alvesrenanpro@gmail.com>
2024-02-18 10:31:31 -0300

    feat: add options for each question on each stages

--- data/stages.js
+++ data/stages.js
@@ -17,218 +17,98 @@ const data = {
         "answer": "b"
       },
       "2": {
-        "question": "Num sei o que num sei o que?",
+        "image": "static/img/stages/s1q1.png",
+        "image_desc": "Uma estátua alta com dois losangos a rodeando!",
+        "question": "Qual o nome dado ao monumento acima?",
         "options": {
-          "a": "Num se se se",
-          "b": "Num reyery wrwer",
-          "c": "Num hhfghfghfghfghfgh",
-          "d": "Num se sasasd"
+          "a": "Mulher Meditando",
+          "b": "Raspadeira de Mandioca",
+          "c": "Estátua de Pitia",
+          "d": "A Fundadora do Araripe"
         },
-        "answer": "a"
+        "answer": "b"
       },
       "3": {
-        "question": "Num sei o que num sei o que?",
+        "image": "static/img/stages/s1q1.png",
+        "image_desc": "Uma estátua alta com dois losangos a rodeando!",
+        "question": "Qual o nome dado ao monumento acima?",
         "options": {
-          "a": "Num se se se",
-          "b": "Num reyery wrwer",
-          "c": "Num hhfghfghfghfghfgh",
-          "d": "Num se sasasd"
+          "a": "Mulher Meditando",
+          "b": "Raspadeira de Mandioca",
+          "c": "Estátua de Pitia",
+          "d": "A Fundadora do Araripe"
         },
-        "answer": "a"
+        "answer": "b"
       },
       "4": {
-        "question": "Num sei o que num sei o que?",
+        "image": "static/img/stages/s1q1.png",
+        "image_desc": "Uma estátua alta com dois losangos a rodeando!",
+        "question": "Qual o nome dado ao monumento acima?",
         "options": {
-          "a": "Num se se se",
-          "b": "Num reyery wrwer",
-          "c": "Num hhfghfghfghfghfgh",
-          "d": "Num se sasasd"
+          "a": "Mulher Meditando",
+          "b": "Raspadeira de Mandioca",
+          "c": "Estátua de Pitia",
+          "d": "A Fundadora do Araripe"
         },
-        "answer": "a"
+        "answer": "b"
       },
-      "5": {
-        "question": "Num sei o que num sei o que?",
-        "options": {
-          "a": "Num se se se",
-          "b": "Num reyery wrwer",
-          "c": "Num hhfghfghfghfghfgh",
-          "d": "Num se sasasd"
-        }
-      },
-      "answer": "a"
-    }
-  },
-  "figuras_historicas": {
-    "icon": "󱦟",
-    "title": "Figuras Históricas",
-    "description": "A Cultura do Araripe",
-    "questions": {
-      "1": {
-        "question": "Num sei o que num sei o que?",
-        "options": {
-          "a": "Num se se se",
-          "b": "Num reyery wrwer",
-          "c": "Num hhfghfghfghfghfgh",
-          "d": "Num se sasasd"
-        },
-        "answer": "a"
-      },
-      "2": {
-        "question": "Num sei o que num sei o que?",
-        "options": {
-          "a": "Num se se se",
-          "b": "Num reyery wrwer",
-          "c": "Num hhfghfghfghfghfgh",
-          "d": "Num se sasasd"
-        },
-        "answer": "a"
-      },
-      "3": {
-        "question": "Num sei o que num sei o que?",
-        "options": {
-          "a": "Num se se se",
-          "b": "Num reyery wrwer",
-          "c": "Num hhfghfghfghfghfgh",
-          "d": "Num se sasasd"
-        },
-        "answer": "a"
-      },
-      "4": {
-        "question": "Num sei o que num sei o que?",
-        "options": {
-          "a": "Num se se se",
-          "b": "Num reyery wrwer",
-          "c": "Num hhfghfghfghfghfgh",
-          "d": "Num se sasasd"
-        },
-        "answer": "a"
-      },
-      "5": {
-        "question": "Num sei o que num sei o que?",
-        "options": {
-          "a": "Num se se se",
-          "b": "Num reyery wrwer",
-          "c": "Num hhfghfghfghfghfgh",
-          "d": "Num se sasasd"
-        }
-      },
-      "answer": "a"
-    }
+    },
   },
   "historia": {
     "icon": "󱦟",
-    "title": "Cultura",
-    "description": "A Cultura do Araripe",
+    "title": "História",
+    "description": "Vamos conhecer um pouco da história?",
     "questions": {
       "1": {
-        "question": "Num sei o que num sei o que?",
+        "image": "static/img/stages/s1q1.png",
+        "image_desc": "Uma estátua alta com dois losangos a rodeando!",
+        "question": "Qual o nome dado ao monumento acima?",
         "options": {
-          "a": "Num se se se",
-          "b": "Num reyery wrwer",
-          "c": "Num hhfghfghfghfghfgh",
-          "d": "Num se sasasd"
+          "a": "Mulher Meditando",
+          "b": "Raspadeira de Mandioca",
+          "c": "Estátua de Pitia",
+          "d": "A Fundadora do Araripe"
         },
-        "answer": "a"
+        "answer": "b"
       },
       "2": {
-        "question": "Num sei o que num sei o que?",
+        "image": "static/img/stages/s1q1.png",
+        "image_desc": "Uma estátua alta com dois losangos a rodeando!",
+        "question": "Qual o nome dado ao monumento acima?",
         "options": {
-          "a": "Num se se se",
-          "b": "Num reyery wrwer",
-          "c": "Num hhfghfghfghfghfgh",
-          "d": "Num se sasasd"
+          "a": "Mulher Meditando",
+          "b": "Raspadeira de Mandioca",
+          "c": "Estátua de Pitia",
+          "d": "A Fundadora do Araripe"
         },
-        "answer": "a"
+        "answer": "b"
       },
       "3": {
-        "question": "Num sei o que num sei o que?",
+        "image": "static/img/stages/s1q1.png",
+        "image_desc": "Uma estátua alta com dois losangos a rodeando!",
+        "question": "Qual o nome dado ao monumento acima?",
         "options": {
-          "a": "Num se se se",
-          "b": "Num reyery wrwer",
-          "c": "Num hhfghfghfghfghfgh",
-          "d": "Num se sasasd"
+          "a": "Mulher Meditando",
+          "b": "Raspadeira de Mandioca",
+          "c": "Estátua de Pitia",
+          "d": "A Fundadora do Araripe"
         },
-        "answer": "a"
+        "answer": "b"
       },
       "4": {
-        "question": "Num sei o que num sei o que?",
+        "image": "static/img/stages/s1q1.png",
+        "image_desc": "Uma estátua alta com dois losangos a rodeando!",
+        "question": "Qual o nome dado ao monumento acima?",
         "options": {
-          "a": "Num se se se",
-          "b": "Num reyery wrwer",
-          "c": "Num hhfghfghfghfghfgh",
-          "d": "Num se sasasd"
+          "a": "Mulher Meditando",
+          "b": "Raspadeira de Mandioca",
+          "c": "Estátua de Pitia",
+          "d": "A Fundadora do Araripe"
         },
-        "answer": "a"
+        "answer": "b"
       },
-      "5": {
-        "question": "Num sei o que num sei o que?",
-        "options": {
-          "a": "Num se se se",
-          "b": "Num reyery wrwer",
-          "c": "Num hhfghfghfghfghfgh",
-          "d": "Num se sasasd"
-        }
-      },
-      "answer": "a"
-    }
-  },
-  "cultura_geral": {
-    "icon": "󱦟",
-    "title": "Cultura",
-    "description": "A Cultura do Araripe",
-    "questions": {
-      "1": {
-        "question": "Num sei o que num sei o que?",
-        "options": {
-          "a": "Num se se se",
-          "b": "Num reyery wrwer",
-          "c": "Num hhfghfghfghfghfgh",
-          "d": "Num se sasasd"
-        },
-        "answer": "a"
-      },
-      "2": {
-        "question": "Num sei o que num sei o que?",
-        "options": {
-          "a": "Num se se se",
-          "b": "Num reyery wrwer",
-          "c": "Num hhfghfghfghfghfgh",
-          "d": "Num se sasasd"
-        },
-        "answer": "a"
-      },
-      "3": {
-        "question": "Num sei o que num sei o que?",
-        "options": {
-          "a": "Num se se se",
-          "b": "Num reyery wrwer",
-          "c": "Num hhfghfghfghfghfgh",
-          "d": "Num se sasasd"
-        },
-        "answer": "a"
-      },
-      "4": {
-        "question": "Num sei o que num sei o que?",
-        "options": {
-          "a": "Num se se se",
-          "b": "Num reyery wrwer",
-          "c": "Num hhfghfghfghfghfgh",
-          "d": "Num se sasasd"
-        },
-        "answer": "a"
-      },
-      "5": {
-        "question": "Num sei o que num sei o que?",
-        "options": {
-          "a": "Num se se se",
-          "b": "Num reyery wrwer",
-          "c": "Num hhfghfghfghfghfgh",
-          "d": "Num se sasasd"
-        }
-      },
-      "answer": "a"
-    }
+    },
   }
-};
+}
 
 export default data;
--- src/jogar.js
+++ src/jogar.js
@@ -4,29 +4,40 @@ import utils from "./utils.js";
 const stagesEl = document.getElementById("stages");
 const gameEl = document.getElementById("game");
 
-if (!localStorage.getItem("stages")) {
-    const obj = {};
-    Object.keys(stages).forEach((key) => {
-        obj[key] = [];
-    })
+const K_STAGES = "stages";
 
-    localStorage.setItem("stages", JSON.stringify(obj));
+if (!localStorage.getItem(K_STAGES)) {
+  const obj = {};
+  Object.keys(stages).forEach((key) => {
+    obj[key] = [];
+  });
+  localStorage.setItem(K_STAGES, JSON.stringify(obj));
 }
 
-function makeQuiz(stage) {
+function makeQuiz(stageKey) {
+  const stage = stages[stageKey];
+  const questions = stage["questions"];
+  const quizContainer = document.createElement("div");
+
+  quizContainer.className = "quiz";
+
+  Object.keys(questions).forEach((key) => {
+    const question = questions[key];
+
     const container = document.createElement("div");
-    container.className = "quiz";
+    container.className = "question";
+    container.id = `question-${key}`;
 
     const image = document.createElement("img");
-    if (stage.questions["1"].image) {
-        image.className = "image";
-        image.alt = stage.questions["1"]["image_desc"]
-        image.src = stage.questions["1"].image;
+    if (question["image"] && question["image_desc"]) {
+      image.className = "image";
+      image.alt = question["image_desc"]
+      image.src = question.image;
     }
-    
+
     const enunciate = document.createElement("h3");
     enunciate.className = "enunciate text-center mv-2"
-    enunciate.innerHTML = `<span class="mark">1.</span> ${stage.questions["1"].question}`;
+    enunciate.innerHTML = `<span class="mark">${key}.</span> ${question["question"]}`;
 
     const form = document.createElement("form");
     form.className = "form";
@@ -36,39 +47,40 @@ function makeQuiz(stage) {
 
     let lastOption = null;
 
-    Object.keys(stage.questions["1"].options).forEach((letter) => {
-        const optionContainer = document.createElement("div");
-        optionContainer.className = "option";
+    Object.keys(question["options"]).forEach((letter) => {
+      console.log(letter);
 
-        const optionInput = document.createElement("input");
-        optionInput.value = letter;
-        optionInput.className = "option-input"
-        optionInput.type = "radio";
-        optionInput.name = "answer";
-        optionInput.id = letter;
+      const optionContainer = document.createElement("div");
+      optionContainer.className = "option";
 
-        if (optionInput.checked) {
-            optionContainer.classList.add("selected");
+      const optionInput = document.createElement("input");
+      optionInput.value = letter;
+      optionInput.className = "option-input"
+      optionInput.type = "radio";
+      optionInput.name = "answer";
+      optionInput.id = letter;
+
+      if (optionInput.checked) {
+        optionContainer.classList.add("selected");
+      }
+
+      const optionLabel = document.createElement("label");
+      optionLabel.setAttribute("for", letter);
+      optionLabel.textContent = question["options"][letter];
+      optionLabel.className = "option-label";
+
+      optionInput.addEventListener("change", () => {
+        if (lastOption != null) {
+          lastOption.classList.remove("selected");
         }
+        optionContainer.classList.add("selected");
+        lastOption = optionContainer;
+      });
 
-        const optionLabel = document.createElement("label");
-        optionLabel.setAttribute("for", letter);
-        optionLabel.textContent = stage.questions["1"].options[letter];
-        optionLabel.className = "option-label";
+      optionContainer.appendChild(optionInput);
+      optionContainer.appendChild(optionLabel);
 
-        optionInput.addEventListener("change", () => {
-            if (lastOption != null) {
-                lastOption.classList.remove("selected");
-            }
-            optionContainer.classList.add("selected");
-            lastOption = optionContainer;
-        });
-
-
-        optionContainer.appendChild(optionInput);
-        optionContainer.appendChild(optionLabel);
-
-        options.appendChild(optionContainer);
+      options.appendChild(optionContainer);
     });
 
     const panel = document.createElement("p");
@@ -88,19 +100,22 @@ function makeQuiz(stage) {
     submit.textContent = "Enviar";
 
     form.addEventListener("submit", (event) => {
-        event.preventDefault();
-        const answer = (new FormData(event.target)).get("answer");
-        if (!answer) {
-            utils.alert(panel, "Selecione uma alternativa!", "error");
-            return;
-        }
-        if (answer === stage.questions["1"].answer) {
-            utils.alert(panel, "Parabéns! Você Acertou!", "success");
-            utils.storeCorrect("arquitetura", "1")
-        } else {
-            utils.alert(panel, "Resposta errada!", "error");
-            return;
-        }
+      event.preventDefault();
+      const answer = (new FormData(event.target)).get("answer");
+      if (!answer) {
+        utils.alert(panel, "Selecione uma alternativa!", "error");
+        return;
+      }
+      if (answer === question.answer) {
+        utils.alert(panel, "Parabéns! Você Acertou!", "success");
+        utils.storeCorrect(stageKey, key);
+      } else {
+        utils.alert(panel, "Resposta errada!", "error");
+      }
+
+      setTimeout(() => {
+        document.getElementById(`question-${key}`).remove()
+      }, 2000);
     })
 
     btnContainer.appendChild(back);
@@ -111,47 +126,49 @@ function makeQuiz(stage) {
     form.append(btnContainer)
 
     if (image.hasAttribute("src")) {
-        container.appendChild(image);
+      container.appendChild(image);
     }
 
     container.appendChild(enunciate);
     container.appendChild(form);
 
-    return container;
+    quizContainer.appendChild(container);
+  });
+
+  return quizContainer;
 }
 
 function loadStage(stageKey) {
-    if (!stages[stageKey]) {
-        return null;
-    }
+  if (!stages[stageKey]) {
+    return;
+  }
 
-    stagesEl.style.display = "none";
-    gameEl.innerHTML = `<h1 class="anim-slide-fade">PREPARE-SE!</h1>`;
+  stagesEl.style.display = "none";
+  gameEl.innerHTML = `<h1 class="anim-slide-fade">PREPARE-SE!</h1>`;
 
-    setTimeout(() => {
-        gameEl.innerHTML = "";
-        gameEl.appendChild(makeQuiz(stages[stageKey]));
-    }, 2000)
+  setTimeout(() => {
+    gameEl.innerHTML = "";
+    gameEl.appendChild(makeQuiz(stageKey));
+  }, 2000)
 }
 
 function buildStages() {
-    Object.keys(stages).forEach((key) => {
-        const stage = stages[key];
-        const card = document.createElement("div");
-        card.className = "column card anim-slide-fade";
-        card.innerHTML = `
+  Object.keys(stages).forEach((key) => {
+    const stage = stages[key];
+    const card = document.createElement("div");
+    card.className = "column card anim-slide-fade";
+    card.innerHTML = `
             <i class="s-5 thin center mv-1">${stage.icon}</i>
             <h3 class="center text-center">${stage.title}</h3>
             <p class="center text-center">${stage.description}</p>
         `;
 
-        card.addEventListener("click", () => {
-            loadStage(key);
-        });
-
-        stagesEl.appendChild(card);
+    card.addEventListener("click", () => {
+      loadStage(key);
     });
+
+    stagesEl.appendChild(card);
+  });
 }
 
 buildStages();
-loadStage(0)
