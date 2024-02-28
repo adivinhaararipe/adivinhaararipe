92748a2316ed8df60052ab51875d990c8e1a4423
Renan <alvesrenanpro@gmail.com>
2024-02-27 21:35:32 -0300

    feat: questions for arquitetura

--- data/stages.js
+++ data/stages.js
@@ -1,15 +1,15 @@
 const data = {
   "arquitetura": {
-    "icon": "󱦟",
+    "icon": "󱎱",
     "title": "Arquitetura",
     "description": "Você conhece estes lugares do nosso Araripe?",
     "questions": {
       "1": {
         "image": "static/img/stages/s1q1.png",
-        "image_desc": "Uma estátua alta com dois losangos a rodeando!",
+        "image_desc": "Uma estátua alta com dois losangos a rodeando.",
         "question": "Qual o nome dado ao monumento acima?",
         "options": {
-          "a": "Mulher Meditando",
+          "a": "A Mulher Meditando",
           "b": "Raspadeira de Mandioca",
           "c": "Estátua de Pitia",
           "d": "A Fundadora do Araripe"
@@ -17,38 +17,50 @@ const data = {
         "answer": "b"
       },
       "2": {
-        "image": "static/img/stages/s1q1.png",
-        "image_desc": "Uma estátua alta com dois losangos a rodeando!",
-        "question": "Qual o nome dado ao monumento acima?",
+        "image": "static/img/stages/s1q2.png",
+        "image_desc": "Uma casa branca com detalhes azuis!",
+        "question": "Que nome recebe a histórica casa da imagem?",
         "options": {
-          "a": "Mulher Meditando",
-          "b": "Raspadeira de Mandioca",
-          "c": "Estátua de Pitia",
-          "d": "A Fundadora do Araripe"
+          "a": "Casa de Carlinhos",
+          "b": "Casarão de Pitia",
+          "c": "Casa de José de Loiola",
+          "d": "Casa Instrumental"
         },
-        "answer": "b"
+        "answer": "a"
       },
       "3": {
-        "image": "static/img/stages/s1q1.png",
-        "image_desc": "Uma estátua alta com dois losangos a rodeando!",
-        "question": "Qual o nome dado ao monumento acima?",
+        "image": "static/img/stages/s1q3.png",
+        "image_desc": "Um padre muito conhecido na localidade.",
+        "question": "O monumento cultural da imagem é a representação de:",
         "options": {
-          "a": "Mulher Meditando",
-          "b": "Raspadeira de Mandioca",
-          "c": "Estátua de Pitia",
-          "d": "A Fundadora do Araripe"
+          "a": "Pe. José Humberto de Lima",
+          "b": "Pe. Antônio Henrique",
+          "c": "Frei Damião",
+          "d": "Santo Antônio"
         },
-        "answer": "b"
+        "answer": "c"
       },
       "4": {
-        "image": "static/img/stages/s1q1.png",
-        "image_desc": "Uma estátua alta com dois losangos a rodeando!",
-        "question": "Qual o nome dado ao monumento acima?",
+        "image": "static/img/stages/s1q4.webp",
+        "image_desc": "O coreto que situa-se ao lado da igreja matriz.",
+        "question": "O Coreto Municipal, grande símbolo do Araripe, foi contruído na época do mandato de:",
         "options": {
-          "a": "Mulher Meditando",
-          "b": "Raspadeira de Mandioca",
-          "c": "Estátua de Pitia",
-          "d": "A Fundadora do Araripe"
+          "a": "José Loiola de Alencar",
+          "b": "Raimundo Elesbão",
+          "c": "Antonio Lima Albuquerque",
+          "d": "Sebastião Afonso de Alencar"
+        },
+        "answer": "a"
+      },
+      "5": {
+        "image": "static/img/stages/s1q5.jpg",
+        "image_desc": "Uma contrução que possui vários comércios!",
+        "question": "A fotografia acima, efetuada a muitos anos atrás, retrata que obra arquitetônica do Araripe?",
+        "options": {
+          "a": "Casarão central",
+          "b": "Mercado Público",
+          "c": "Edifício Central",
+          "d": "Prefeitura Municipal"
         },
         "answer": "b"
       },
---
+++ questoes.md
@@ -0,0 +1,67 @@
+# Stage 1: Arquitetura
+
+1. Qual o nome do monumento acima
+[X] Raspadeira de mandioca
+
+2. Que nome recebe a histórica casa da imagem?
+[X] Casa de Carlinhos
+
+3. O monumento cultural da imagem é a representação de:
+[X] Frei Damião
+
+4. O Coreto Municipal, grande símbolo do Araripe, foi contruído na época do mandato de:
+[X] José Loiola de Alencar
+
+5. A fotografia acima, efetuada a muitos anos atrás, retrara que obra arquitetônica do Araripe?
+[X] Mercado Público
+
+# Stage 2: Figuras históricas
+
+1. Foi o primeiro prefeito do Araripe a ser eleito por voto popular. Na sua administração foram contruidos o Hospital Municipal e o Mercado Público:
+[X] José Loiola de Alencar
+
+2. Um grande nome da política nacional. Foi economista, advogado, governador do estado de Pernambuco, tendo o nome escrito no Livro dos Heróis da Pátria. Este é:
+[X] Miguel Arraes
+
+3. Foi uma exímia socióloga e ativista político-cultural. Em 1988, assumiu a Secretaria de Cultura do Estado do Ceará. Ela é:
+[X] Violeta Arraes
+
+4. Uma figura histórica do município. Após uma decepção amorosa, tornou-se uma pessoa privada. Viveu em um casarão que recebe seu nome. Esta mulher é:
+[X] Pitia
+
+5. Poeta nascido no Sítio Riacho Grande, foi prefeito no ano de 1971, sendo também compositor do hino do Araripe:
+[X] Raimundo Elesbão
+
+# Stage 3: História
+
+1. Em que ano foi fundado oficialmente o município de Araripe?
+[X] 1875
+
+2. Antes de ser chamada de Araripe, a localidade foi, inicialmente, chamada de:
+[X] Brejo Seco
+
+3. Qual o nome do padree que motivou uma maior povoação da localidade de Brejo Seco?
+[X] Henrique José Cavalcante
+
+4. Onde se localizava a igreja onde foi realizada a primeira missa do povoado de Brejo Seco?
+[X] Rua Alexandre Arraes
+
+5. Atualmente, o município de araripe possui, além do distrito-sede, quatro outros distritos, que são:
+[X] Brejinho, Riacho Grande, Pajeú e Alagoinha
+
+# Stage 4: Eventos
+
+1. A competição que reúne quadrilhas das escolas de todo o município para comemorar as festas de São João é chamada de:
+[X] Fequaripe
+
+2. Em que mês do ano ocorre a festa do padroeiro na sede municipal?
+[X] Junho
+
+3. O evento público que ocorre em 3 dias do mês de agosto e conta com vários artistas na sede é chamado de:
+[X] Festa do Município
+
+4. Qual o padroeiro do município de Araripe?
+[X] Santo Antônio
+
+5. Qual o nome do evento que reúne artistas de todo município para apresentação de seus trabalhos?
+[X] Praxedes 
